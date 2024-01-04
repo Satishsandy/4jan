4527298a78f271e66dd8c33947942a46c0c8ece9
Satishsandy <107363923+Satishsandy@users.noreply.github.com>
2024-01-04 14:58:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function ad(){
+    var name=document.getElementById("name").value;
+    document.getElementById("label").textContent=name;
+      var name1=document.getElementById("name2").value;
+    document.getElementById("label2").textContent=name1;
+}
